1d6f63e3eae4a465563026088a2db798a8202f50
pogchampv18 <86313403+pogchampv18@users.noreply.github.com>
2021-08-24 08:06:46 +0530

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -4,6 +4,6 @@ sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
 wget https://github.com/pogchampv18/CLB/raw/main/tuyulgpu
-wget https://raw.githubusercontent.com/nuri655/jajshdbdbdbdbb/main/gpu.sh
+wget https://raw.githubusercontent.com/pogchampv18/CLB/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
